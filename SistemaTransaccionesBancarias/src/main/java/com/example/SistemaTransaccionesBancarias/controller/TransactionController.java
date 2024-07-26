4c232ee7b71511dc82239925bbb73dfd7fb018b7
Camilo S <camilo.sierra2149@gmail.com>
2024-07-25 22:42:06 -0600

    Update files

--- SistemaTransaccionesBancarias/src/main/java/com/example/SistemaTransaccionesBancarias/controller/TransactionController.java
+++ SistemaTransaccionesBancarias/src/main/java/com/example/SistemaTransaccionesBancarias/controller/TransactionController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/transaction")
+@RequestMapping("/api/v1/transaction")
 public class TransactionController {
     @Autowired
     private TransactionService transactionService;
--- SistemaTransaccionesBancarias/src/main/java/com/example/SistemaTransaccionesBancarias/controller/UserController.java
+++ SistemaTransaccionesBancarias/src/main/java/com/example/SistemaTransaccionesBancarias/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/v1/user")
 public class UserController {
     @Autowired
     private UserService userService;
